f84bde842744a32d1b65cb4ee41aa8de0963a0ca
nhminh22@clc.fitus.edu.vn <nguyenhuuminh8204@gmail.com>
2025-03-12 23:38:32 +0700

    Initial Commit

---
+++ TKPM final project/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKPM_final_project.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        //T GetById(string id);
+
+        //int Insert(T entity);
+        //int Update(T entity);
+        //int Delete(T entity);
+    }
+}

c66dccebb7a6fd575fea39fc3a2ca45d7e6ba5b3
nhminh22@clc.fitus.edu.vn <nguyenhuuminh8204@gmail.com>
2025-03-25 00:11:47 +0700

    delete unneccessary files

---
+++ MyTool/MyTool/ReverseStringTool.cs
@@ -0,0 +1,19 @@
+﻿using MyTool.ToolContracts;
+
+namespace MyTool
+{
+    public class ReverseStringTool : ITool
+    {
+        public string Name => "String Reverser";
+        public string Description => "Reverses the input string.";
+
+        public ToolType Type => ToolType.String;
+
+        public string Execute(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ MyTool/MyTool/ToolContracts/ITool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTool.ToolContracts
+{
+    public interface ITool
+    {
+        string Name { get; }
+        string Description { get; }
+
+        ToolType Type { get; }
+        string Execute(string input);
+    }
+}
+
